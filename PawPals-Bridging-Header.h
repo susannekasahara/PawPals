37a5331e83ef4308b03feb9f91e67569c4e29cfd
Susanne Kasahara <kasaharasusanne@gmail.com>
2015-12-28 01:57:41 -0500

    updates

--- PawPals-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "AFNetworking.h"
-#import "AFAmazonS3Manager.h"
-#import "AFAmazonS3ResponseSerializer.h"
